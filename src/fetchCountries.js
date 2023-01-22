24dfbaf3cb6f26eb0f41c2bd3d3cbd0524a0084b
SvitlanaPia <111311701+SvitlanaPia@users.noreply.github.com>
2023-01-23 00:26:11 +0200

    changed-filters-for-test

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,11 +2,11 @@ export class RestcountriesAPI {
   static BASE_URL = 'https://restcountries.com';
 
   fetchCountries(name) {
-    const searchParams = new URLSearchParams({
-      fields: ['name', 'capital', 'population', 'flags', 'languages'],
-    });
+    // const searchParams = new URLSearchParams({
+    //   fields: ['name', 'capital', 'population', 'flags', 'languages'],
+    // });
     return fetch(
-      `${RestcountriesAPI.BASE_URL}/v3.1/name/${name}?${searchParams}`
+      `${RestcountriesAPI.BASE_URL}/v3.1/name/${name}?fields=name,capital,population,flags,languages`
     ).then(response => {
       if (!response.ok) {
         throw new Error(response.status);
